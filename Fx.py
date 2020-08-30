1923f65c34bfb1127696c7c634560133eac8fe60
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2020-08-30 17:46:45 +0530

    ...

---
+++ Fx.py
@@ -0,0 +1,7 @@
+
+# Start welcome function
+def start ():
+    print("=> Hello, This is SPRITAN a chatbot AI")
+    print("=> Feel free to say hi or command to do something")
+    print("-------------------------------------------------------------------------------------")
+
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # imports
 from Text_to_Voice import speak_it
+from Fx import start
+
+# Global Variables
+requestAI=""
+requestAIs=""
+i=0
 
 # working
-speak_it("lol")
+start()
+while requestAIs!="exit":
+    flag = 0
+    requestAI=input("You     => ")
+    requestAIs=requestAI.lower() #can be changed latter
+    
