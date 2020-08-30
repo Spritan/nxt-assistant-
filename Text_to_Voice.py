05052ce8de93e7fea1c6db00899064a2676c2e14
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2020-08-30 16:11:00 +0530

    text ti voice

---
+++ Text_to_Voice.py
@@ -0,0 +1,16 @@
+# importing the pyttsx library
+import pyttsx3
+
+# initialisation
+engine = pyttsx3.init()
+
+# testing
+
+#    engine.say("My first code on text-to-speech")
+#    engine.runAndWait()
+
+# function
+
+def speak_it (op):
+    engine.say(op)
+    engine.runAndWait()
---
+++ main.py
@@ -0,0 +1,5 @@
+# imports
+from Text_to_Voice import speak_it
+
+# working
+speak_it("lol")
