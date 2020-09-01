0c3ac9a752273b6e69b7a46a1040325d60a0789a
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2020-09-01 21:42:36 +0530

    user class process started

---
+++ User.py
@@ -0,0 +1,18 @@
+
+import pickle
+
+class user_add:
+    def __init__(self,user_name,user_password):
+        self.user_name=user_name 
+        self.user_password=user_password
+
+'''
+p1 = user_add("John", 36)
+
+print(p1.user_name)
+print(p1.user_password)
+'''
+my = MyClass("someone")
+pickle.dump(my, open("myobject", "wb"))
+me = pickle.load(open("myobject", "rb"))
+me.display()
