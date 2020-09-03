4246285eb75052dc628298e8b8d08f0f00c9a991
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2020-09-03 19:10:49 +0530

    Update Sql_connection.py

--- Sql_connection.py
+++ Sql_connection.py
@@ -1,5 +1,6 @@
 # imports
 import mysql.connector
+from Text_to_Voice import speak_it
 
 # configuration
 mydb=mysql.connector.connect(
@@ -13,10 +14,25 @@ mydb=mysql.connector.connect(
 mycursor=mydb.cursor()
 
 sqlformula="INSERT INTO respond (ques,ans) VALUES (%s, %s)"
+sqlformula2 = "SELECT ans FROM respond WHERE ques = "
 
 #fuctions to input
 def add_respond(u_input,p_output):
     a1 = (u_input, p_output)
     mycursor.execute(sqlformula,a1)
     mydb.commit()
-# add_respond("is this heaven?","Plz refer to God")
+
+#function to output
+def get_respond(u_input):
+    a2 = sqlformula2+"'"+u_input+"'"
+    mycursor.execute(a2)
+    myresult=mycursor.fetchone()
+    return myresult
+
+
+p_output = get_respond("are you happy?")
+speak_it(p_output)
+print(p_output)
+
+
+
