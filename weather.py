eba28c5c2478e9a98f11aad055f13c715d686123
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2020-09-04 10:40:10 +0530

    wather open activated

--- weather.py
+++ weather.py
@@ -7,69 +7,77 @@
 
 # import required modules
 import requests, json
+from Text_to_Voice import speak_it
 
-# Enter your API key here
+# Enter your API key here & base_url variable to store url
 api_key = "3084c7527f2b6278583b46676e5b8556"
-
-# base_url variable to store url
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-# Give city name
-city_name = input("Enter city name : ")
+# function to get weather
+def weather_report(city_name='Golaghat'):
+    # complete_url variable to store
+    # complete url address
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
-# complete_url variable to store
-# complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
 
-# get method of requests module
-# return response object
-response = requests.get(complete_url)
+    # json method of response object
+    # convert json format data into
+    # python format data
+    x = response.json()
 
-# json method of response object
-# convert json format data into
-# python format data
-x = response.json()
+    # Now x contains list of nested dictionaries
+    # Check the value of "cod" key is equal to
+    # "404", means city is found otherwise,
+    # city is not found
+    if x["cod"] != "404":
 
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
-if x["cod"] != "404":
+        # store the value of "main"
+        # key in variable y
+        y = x["main"]
 
-    # store the value of "main"
-    # key in variable y
-    y = x["main"]
+        # store the value corresponding
+        # to the "temp" key of y
+        current_temperature = y["temp"]
 
-    # store the value corresponding
-    # to the "temp" key of y
-    current_temperature = y["temp"]
+        # store the value corresponding
+        # to the "pressure" key of y
+        current_pressure = y["pressure"]
 
-    # store the value corresponding
-    # to the "pressure" key of y
-    current_pressure = y["pressure"]
+        # store the value corresponding
+        # to the "humidity" key of y
+        current_humidiy = y["humidity"]
 
-    # store the value corresponding
-    # to the "humidity" key of y
-    current_humidiy = y["humidity"]
+        # store the value of "weather"
+        # key in variable z
+        z = x["weather"]
 
-    # store the value of "weather"
-    # key in variable z
-    z = x["weather"]
+        # store the value corresponding
+        # to the "description" key at
+        # the 0th index of z
+        weather_description = z[0]["description"]
 
-    # store the value corresponding
-    # to the "description" key at
-    # the 0th index of z
-    weather_description = z[0]["description"]
+        # output following values
+        print("SPRITAN => Temperature (in kelvin unit)       = " +
+              str(current_temperature) +
+              "\n\t   atmospheric pressure (in hPa unit) = " +
+              str(current_pressure) +
+              "\n\t   humidity (in percentage)           = " +
+              str(current_humidiy) +
+              "\n\t   description                        = " +
+              str(weather_description))
 
-    # print following values
-    print(" Temperature (in kelvin unit) = " +
-          str(current_temperature) +
-          "\n atmospheric pressure (in hPa unit) = " +
-          str(current_pressure) +
-          "\n humidity (in percentage) = " +
-          str(current_humidiy) +
-          "\n description = " +
-          str(weather_description))
+        speak_it(" Temperature (in kelvin unit) = " +
+                 str(current_temperature) +
+                 "\n atmospheric pressure (in hPa unit) = " +
+                 str(current_pressure) +
+                 "\n humidity (in percentage) = " +
+                 str(current_humidiy) +
+                 "\n description = " +
+                 str(weather_description))
 
-else:
-    print(" City Not Found ")
+    else:
+        speak_it(" City Not Found ")
+        print("SPRITAN => City Not Found ")
